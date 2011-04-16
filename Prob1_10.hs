efeb97daa2c85b46b114413f46765e6496a52acc
Jose Manuel Torres <josemanuel.tch@gmail.com>
2011-04-16 14:07:41 -0500

    sort !

--- Prob1_10.hs
+++ Prob1_10.hs
@@ -347,7 +347,8 @@ the possibilities and returns them in a list.
 b) Generalize the above predicate in a way that we can specify a list
 of group sizes and the predicate will return a list of groups. -}
 
-
+-- Later
+-- myGroup :: [Int] -> [[a]] -> [[[[a]]]]
 
 
 {- Problem 28
@@ -359,12 +360,24 @@ themselves. The objective is to sort the elements of this list
 according to their length. E.g. short lists first, longer lists later,
 or vice versa. 
 
+-}
+
+lsort :: [[a]] -> [[a]]
+lsort [] = []
+lsort (x:[]) = x:[]
+lsort (x:xs) = 
+    let stail = lsort xs
+    in if (myLenght x) <= ( (myLenght.head)stail) then  x:stail
+       else (head stail):(lsort (x:(tail stail)) )
+    
+{-
+
 b) Again, we suppose that a list contains elements that are lists
 themselves. But this time the objective is to sort the elements of
 this list according to their length frequency; i.e., in the default,
 where sorting is done ascendingly, lists with rare lengths are placed
 first, others with a more frequent length come later. -}
 
-
+-- later
 
 
